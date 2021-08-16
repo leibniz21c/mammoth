bcc67b5be075b7dc055c6df08eb21112608add69
Heesung Yang <leibniz21c@gmail.com>
2021-08-17 01:21:34 +0900

    Update conf.py

--- monitor/conf.py
+++ monitor/conf.py
@@ -4,5 +4,5 @@ config = {
     "COLLECTOR_URL": "http://slb-7695562.ncloudslb.com",
     "COLLECTOR_PORT": 20000,
     "COLLECTOR_USER_CONF": "/userconf",
-    "GITHUB": "https://github.com/ndo04343/container-based-hadoop-distributed-system-log-analyzer/tree/main/monitor"
+    "GITHUB": "https://github.com/ndo04343/mammoth"
 }
